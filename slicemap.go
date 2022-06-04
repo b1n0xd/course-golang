4462be9b886951c511938b07591f4c416d5f7e5d
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-04 16:19:16 -0300

    Course Golang.

---
+++ slicemap.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	a1 := [3]int{1, 2, 3}
+	s1 := []int{1, 2, 3}
+	fmt.Println(a1, s1)
+	fmt.Println(reflect.TypeOf(a1), reflect.TypeOf(s1))
+
+	a2 := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(reflect.TypeOf(a2))
+	// Slice nao é um array, Slice define um pedaco de um array.
+
+	s2 := a2[1:3]
+	fmt.Println(a2, s2)
+
+	s3 := a2[:2]
+	fmt.Println(a2, s3)
+	// Slice: tamanho e um ponteiro para um elemento de um array.
+
+	s4 := s2[:1]
+	fmt.Println(s4)
+}
