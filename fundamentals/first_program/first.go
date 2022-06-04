cd8a9feeab9a97be8c6583d3fdca565d62874990
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-04 16:54:27 -0300

    Course Golang, first programa create to tranning.

---
+++ fundamentals/first_program/first.go
@@ -0,0 +1,15 @@
+// Programas executaveis iniciam pelo pacote main
+package main
+
+/*
+Os codigos em GO sao organizados em pacotes
+e para usa-los e necessario declarar um ou varios imports
+*/
+import "fmt"
+
+// A porta de entregada de um programa em GO é a funcao main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+
+}
